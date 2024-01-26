bf4ed608770a13434fa0c8be5f8afb9dd932722a
xedter20 <dextermiranda441@gmail.com>
2024-01-26 21:48:44 +0800

    sendgrid update

--- helpers/emailSending.js
+++ helpers/emailSending.js
@@ -1,10 +1,9 @@
 import sgMail from '@sendgrid/mail';
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
 export const sendEmail = async ({ data }) => {
   let link = data.link;
 
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   let mailOptions = {
     from: 'admin@axztechItsolutions.com',
     to: 'admin@amuletinternational-official.com',
